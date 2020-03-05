69388a5e546c033e01129f59cf3fc2d9240c7018
Mirek Rusin <mirek@me.com>
2020-03-05 12:12:47 +0100

    Adding support for custom, per-project "plugin".

--- bin/changelog.js
+++ bin/changelog.js
@@ -1,17 +1,24 @@
 #! /usr/bin/env node
 
+const { resolve } = require('path')
+const { existsSync } = require('fs')
 const { execSync } = require('child_process')
 
 const isSemver = _1 => /^v?\d+\.\d+\.\d+$/.test(_1)
 const puts = console.log
 const sh = cmd => execSync(cmd, { encoding: 'utf8' }).trim()
-const linesOf = cmd => sh(cmd).split('\n').map(_1 => _1.trim()).filter(Boolean)
 const tagsOf = () => linesOf('git tag --sort=-creatordate').filter(isSemver)
 const dateOf = tag => sh(`git log -1 --format=%ai ${tag} | awk '{ print $1 }'`)
-const logsOf = (tag1, tag2) => linesOf(`git log ${tag1}...${tag2} --pretty=format:'%s'`).filter(Boolean)
 const withDot = $1 => $1.endsWith('.') ? $1 : `${$1}.`
 const withCapital = $1 => $1.charAt(0).toUpperCase() + $1.slice(1)
 const sentenceOf = $1 => withDot(withCapital($1))
+const linesOf = cmd => sh(cmd).split('\n').map(_1 => _1.trim()).filter(Boolean)
+const logsOf = (tag1, tag2) => linesOf(`git log ${tag1}...${tag2} --pretty=format:'%s'`).filter(Boolean)
+
+const pluginPath = resolve('./.changelog.js')
+const plugin = existsSync(pluginPath) ?
+  require(pluginPath) :
+  { transform: _ => _ }
 
 const isValuableLog = log => {
   if (typeof log !== 'string') {
@@ -44,6 +51,6 @@ for (let i = 0; i < tags.length - 1; i++) {
     logs = [ 'No changes.' ]
   }
   for (const log of logs) {
-    puts(`* ${sentenceOf(log)}`)
+    puts(`* ${plugin.transform(sentenceOf(log))}`)
   }
 }
